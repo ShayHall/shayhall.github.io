2ce4ff0bc436c18a40af5c76560d3c4f5d8af5b1
Shay-Hall <13802039+ShayHall@users.noreply.github.com>
2026-02-13 18:20:54 -0500

    Make secondary nav the tabs

--- js/main.js
+++ js/main.js
@@ -144,7 +144,31 @@ document.addEventListener('keydown', (e)=>{
 
 
 /* tabs */
+function tabAction() {
+  const id=this.getAttribute("data-id");
+  const active_nav = document.getElementsByClassName("tab-nav-item_active");
+  while (active_nav.length > 0) {
+    active_nav[0].classList.remove("tab-nav-item_active");
+  };
+
+  this.classList.add("tab-nav-item_active");
+  
+  const active_content = document.getElementsByClassName("tab-content-item_active");
+  while (active_content.length > 0) {
+    active_content[0].classList.remove("tab-content-item_active");
+  }
+  document.getElementById(id).classList.add("tab-content-item_active");
+}
+
+if ('navigation' in window) {
+  window.navigation.addEventListener('navigate', (event) => {
+    handleLinkClick();
+  });
+}
+
 const currentPath = window.location.pathname;
+
+function handleLinkClick() {
 
 if (currentPath === "/") {
   setTimeout(() => {
@@ -153,32 +177,21 @@ if (currentPath === "/") {
       const nav_items = document.getElementsByClassName("tab-nav-item");
 
       for(let i=0; i < nav_items.length; i++) {
-        nav_items[i].addEventListener("click", function() {
-          const id=this.getAttribute("data-id");
-          const active_nav = document.getElementsByClassName("tab-nav-item_active");
-          while (active_nav.length > 0) {
-            active_nav[0].classList.remove("tab-nav-item_active");
-          };
-
-          this.classList.add("tab-nav-item_active");
-          
-          const active_content = document.getElementsByClassName("tab-content-item_active");
-          while (active_content.length > 0) {
-            active_content[0].classList.remove("tab-content-item_active");
-          }
-          document.getElementById(id).classList.add("tab-content-item_active");
-          
-
-        });// end of click event listener
+        nav_items[i].addEventListener("click", tabAction);
       } // end of for loop
     };// end of tabNav function
-
 
     tabNav();
   }, 1000); // end of setTimeout
 } //end of if statement looking for homepage
+};
 
-    
+handleLinkClick()
+
+
+
+
+
       
         
           
